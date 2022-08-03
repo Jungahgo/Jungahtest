6de0d12d78aca5d4789f3dca0fdb67cf7c6a8519
Jungahgo <kwakjungah0605@gmail.com>
2022-08-04 02:40:30 +0900

    result 제외

---
+++ js/data.js
@@ -0,0 +1,127 @@
+const qnaList = [
+    {
+        q: "1. 정아의 최애 과자는 ABC 초코쿠키이다.",
+        img:"./img/abcChoco.png",
+        a: [
+            {answer : '네', type: 1},
+            {answer : '아니요', type: 0}
+        ]
+    },
+    {
+        q: "2. 정아의 최애 아이스크림은 메로나이다.",
+        img:"./img/icecream.png",
+        a: [
+            {answer : '네', type: 0},
+            {answer : '아니요', type: 1}
+        ]
+    },
+    {
+        q: "3. 정아의 최애 스벅 음료는 자바칩 프라푸치노이다.",
+        img:"./img/starbucks.png",
+        a: [
+            {answer : '네', type: 0},
+            {answer : '아니요', type: 1}
+        ]
+    },
+    {
+        q: "4. 정아는 맥북이 있다.",
+        img:"./img/mac.png",
+        a: [
+            {answer : '네', type: 1},
+            {answer : '아니요', type: 0}
+        ]
+    },
+    {
+        q: "5. 정아의 최애 라면은 너구리이다.",
+        img:"./img/ra.png",
+        a: [
+            {answer : '네', type: 1},
+            {answer : '아니요', type: 0}
+        ]
+    },
+    {
+        q: "6. 정아가 가장 자주 이용하는 지하철은 2호선이다.",
+        img:"./img/sub.png",
+        a: [
+            {answer : '네', type: 0},
+            {answer : '아니요', type: 1}
+        ]
+    },
+    {
+        q: "7. 정아의 생일은 6일이다.",
+        img:"./img/birth.png",
+        a: [
+            {answer : '네', type: 0},
+            {answer : '아니요', type: 1}
+        ]
+    },
+    {
+        q: "8. 정아가 여름에 주로 입는 옷 색깔은 검정이다.",
+        img:"./img/clo.png",
+        a: [
+            {answer : '네', type: 1 },
+            {answer : '아니요', type: 0}
+        ]
+    },
+    {
+        q: "9. 정아의 침대에 있는 그리즐리는 총 3마리이다.",
+        img:"./img/sub.png",
+        a: [
+            {answer : '네', type: 0},
+            {answer : '아니요', type: 1}
+        ]
+    },
+    {
+        q: "10. 정아가 가장 좋아하는 음식 중 하나는 샤브샤브이다.",
+        img:"./img/fav.png",
+        a: [
+            {answer : '네', type: 1},
+            {answer : '아니요', type: 0}
+        ]
+    },
+]
+
+const infoList = [
+    {
+        name: "1",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+    {
+        name: "",
+        desc: ""
+    },
+
+
+]
---
+++ js/start.js
@@ -0,0 +1,106 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
+const endPoint = 11;
+var point = 0;
+
+function calResult(){
+    var point = 0;
+    
+}
+
+function goResult(){
+    qna.style.WebkitAnimation = "fadeOut 1s";
+    qna.style.animation = "fadeout 1s";
+
+    setTimeout(() => {
+        result.style.WebkitAnimation = "fadeIn 0.5s";
+        result.style.animation = "fadeIn 0.5s";
+
+        setTimeout(() => {
+            qna.style.display = "none";
+            result.style.display = "block";
+        }, 450)})
+
+    console.log(point);
+}
+
+function addAnswer(answerText, qIdx, idx){
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('button');
+    answer.classList.add('answerList');
+    answer.classList.add("my-5");
+    answer.classList.add("py-3");
+    answer.classList.add("mx-auto");
+    answer.classList.add("fadeIn");
+    a.appendChild(answer);
+    answer.innerHTML = answerText;
+
+    answer.addEventListener("click",function(){
+        var children = document.querySelectorAll('.answerList');
+
+        for (let i = 0; i<children.length; i++){
+            children[i].disabled = true;
+            children[i].style.WebkitAnimation = "fadeOut 0.5s";
+            children[i].animation = "fadeOut 1s"
+            
+        }
+
+        setTimeout(() => {
+            point = point+qnaList[qIdx].a[idx].type
+
+
+            for (let i = 0; i < children.length; i++){
+                children[i].style.display = 'none';
+            }
+            goNext(++qIdx);
+        },450)
+        
+    },false);
+}
+
+function goNext(qIdx){
+
+    if (qIdx+1 == endPoint){
+        goResult();
+        return;
+    }
+
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdx].q;
+
+    
+    var img = document.createElement("img");
+    img.src = qnaList[qIdx].img;
+    
+    q.appendChild(img);
+
+    for (let i in qnaList[qIdx].a){
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
+    }
+
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx+1) + '%';
+    
+}
+
+function begin(){
+    main.style.WebkitAnimation = "fadeOut 1s";
+    main.style.animation = "fadeout 1s";
+
+    setTimeout(() => {
+        qna.style.WebkitAnimation = "fadeIn 0.5s";
+        qna.style.animation = "fadeIn 0.5s";
+
+        setTimeout(() => {
+            main.style.display = "none";
+            qna.style.display = "block";
+        }, 450);
+        let qIdx = 0;
+        goNext(qIdx);
+    }, 450);
+
+    
+ 
+}
+
